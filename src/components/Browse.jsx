088ec4bd04f61e4a9f287aef6e8ac3e8f77aac35
Atharva Yadav <78128129+atharva-yadav@users.noreply.github.com>
2026-01-19 04:19:26 +0000

    Added themoviedb API in project 191

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants"
 import Header from "./Header"
 
 const Browse = () => {
+
+  const getNowPlayingMovies = async () => {
+    const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS);
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+  }
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, [])
   return <div>
     <Header></Header>
   </div>
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Y2M5YzkyZTI2MGNjZjBiZTg5MGMwNGI4MzA1ODVjNCIsIm5iZiI6MTc2ODc5NTE2Mi45NTEsInN1YiI6IjY5NmRhYzFhOGJmNTkwZTI2ZThmZDFhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1gSybEeIJ-SksKFZg8OitSQ876p-DJc1D7Yaj-5UB9I",
+  },
+};
