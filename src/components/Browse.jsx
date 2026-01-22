160eb34efdc05108ed5b5544b404af64e2114ce8
Atharva Yadav <78128129+atharva-yadav@users.noreply.github.com>
2026-01-22 03:37:31 +0000

    Fetch moviesData from API and added to moviesSlice in store 221

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants"
 import Header from "./Header"
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+import { useDispatch } from "react-redux";
 
 const Browse = () => {
-
+  const dispatch = useDispatch();
+  
   const getNowPlayingMovies = async () => {
     const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS);
     const jsonResponse = await response.json();
+    dispatch(addNowPlayingMovies(jsonResponse));
     console.log(jsonResponse);
   }
 
